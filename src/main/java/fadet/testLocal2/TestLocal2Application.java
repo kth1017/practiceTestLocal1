df8834044267dd28bbd46c7efb210bfe4dafae65
KTH <rnjsxo1017@gmail.com>
2023-12-01 22:02:23 +0900

    1

---
+++ src/main/java/fadet/testLocal2/TestLocal2Application.java
@@ -0,0 +1,13 @@
+package fadet.testLocal2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestLocal2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestLocal2Application.class, args);
+	}
+
+}
---
+++ src/test/java/fadet/testLocal2/TestLocal2ApplicationTests.java
@@ -0,0 +1,13 @@
+package fadet.testLocal2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestLocal2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
